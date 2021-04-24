ec43b122d3c49aa55566351a310cb9a0402a466d
Karen <karenyaa@live.co.uk>
2021-04-24 14:27:06 +0100

    created get route for all workouts

--- routes/api.js
+++ routes/api.js
@@ -2,4 +2,22 @@ const db = require("../models");
 const router = require("express").Router();
 
 //get workouts
+router.get("/api/workouts", (req, res) => {
 
+    db.Workout.find({}).then(dbWorkout => {
+        // console.log("ALL WORKOUTS");
+        // console.log(dbWorkout);
+        dbWorkout.forEach(workout => { //loop through each workout
+            var total = 0;
+            workout.exercises.forEach(e => { //loop through exercises in each workout
+                total += e.duration; //append total workout num to duration of exercise
+            });
+            workout.totalDuration = total; //the total duration of exercise 
+
+        });
+
+        res.json(dbWorkout);
+    }).catch(err => {
+        res.json(err);
+    });
+});
