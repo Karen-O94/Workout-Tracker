f0d6669a3e832c7ddf27c2bd107f17286bbafc04
Karen <karenyaa@live.co.uk>
2021-04-25 21:09:44 +0100

    amended error of file name in html route

--- routes/html.js
+++ routes/html.js
@@ -6,10 +6,10 @@ router.get("/", (req, res) => {
 });
 
 router.get("/exercise", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/exercise.js"));
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
 });
 
-router.get("/stats", (req,res) => {
+router.get("/stats", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/stats.html"));
 });
 
